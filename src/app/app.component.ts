5af7c81181f4cb9d91a809f3bcf15b04de59904a
monkick <saruyama.monki@gmail.com>
2017-06-19 21:25:34 +0900

    ngrx tutorial done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Store} from '@ngrx/store';
+import {INCREMENT, DECREMENT, RESET} from './counter';
+
+interface AppState {
+    counter: number;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector   : 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls  : ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    counter: Observable<number>;
+
+    constructor(private store: Store<AppState>) {
+        this.counter = store.select('counter');
+    }
+
+    increment() {
+        this.store.dispatch({type: INCREMENT});
+    }
+
+    decrement() {
+        this.store.dispatch({type: DECREMENT});
+    }
+
+    reset() {
+        this.store.dispatch({type: RESET});
+    }
 }
